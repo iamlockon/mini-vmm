6e4d84dd7f2c33b7686d2a38105a0272a517dd2e
iamlockon <xdddxyyyxzzz123@gmail.com>
2026-06-06 14:08:56 +0900

    put magic numbers into consts

--- src/linux_loader.rs
+++ src/linux_loader.rs
@@ -28,26 +28,44 @@ const BOOT_PARAMS_ADDR: u64 = 0x9000;
 const CMDLINE_ADDR: u64 = 0x20000;
 const KERNEL_LOAD_ADDR: u64 = 0x100000;
 
+const SECTOR_SIZE: usize = 512;
+const BOOT_PARAMS_SIZE: usize = 4096;
+const DEFAULT_SETUP_SECTS: u8 = 4;
+
+// magic numbers
+const SETUP_SECTS_OFFSET: usize = 0x1f1;
+const BOOT_FLAG_OFFSET: usize = 0x1fe;
+const BOOT_FLAG_MAGIC: u16 = 0xaa55;
+const HEADER_OFFSET: usize = 0x202;
+const HEADER_MAGIC: &[u8; 4] = b"HdrS";
+const PROTOCOL_VERSION_OFFSET: usize = 0x206;
+const LOADFLAGS_OFFSET: usize = 0x211;
+const CODE32_START_OFFSET: usize = 0x214;
+const CMDLINE_PTR_OFFSET: u64 = 0x228;
+const INITRD_ADDR_MAX_OFFSET: usize = 0x22c;
+const KERNEL_ALIGNMENT_OFFSET: usize = 0x230;
+const CMDLINE_SIZE_OFFSET: usize = 0x238;
+
 impl LinuxKernel {
     pub fn parse(image: Vec<u8>) -> Result<Self, Box<dyn Error>> {
-        Self::require_len(&image, 0x238 + 4)?;
+        Self::require_len(&image, CMDLINE_SIZE_OFFSET + 4)?;
 
-        let boot_flag = Self::read_u16(&image, 0x1fe)?;
-        if boot_flag != 0xaa55 {
+        let boot_flag = Self::read_u16(&image, BOOT_FLAG_OFFSET)?;
+        if boot_flag != BOOT_FLAG_MAGIC {
             return Err(format!("invalid boot flag: {boot_flag:#x}").into());
         }
 
-        if &image[0x202..0x206] != b"HdrS" {
+        if &image[HEADER_OFFSET..HEADER_OFFSET + HEADER_MAGIC.len()] != HEADER_MAGIC {
             return Err("missing Linux boot protocol header: HdrS".into());
         }
 
-        let raw_setup_sects = image[0x1f1];
+        let raw_setup_sects = image[SETUP_SECTS_OFFSET];
         let setup_sects = if raw_setup_sects == 0 {
-            4
+            DEFAULT_SETUP_SECTS
         } else {
             raw_setup_sects
         };
-        let setup_size = (setup_sects as usize + 1) * 512;
+        let setup_size = (setup_sects as usize + 1) * SECTOR_SIZE;
 
         if image.len() <= setup_size {
             return Err(format!(
@@ -59,12 +77,12 @@ impl LinuxKernel {
         }
 
         Ok(Self {
-            protocol_version: Self::read_u16(&image, 0x206)?,
-            loadflags: image[0x211],
-            code32_start: Self::read_u32(&image, 0x214)?,
-            initrd_addr_max: Self::read_u32(&image, 0x22c)?,
-            kernel_alignment: Self::read_u32(&image, 0x230)?,
-            cmdline_size: Self::read_u32(&image, 0x238)?,
+            protocol_version: Self::read_u16(&image, PROTOCOL_VERSION_OFFSET)?,
+            loadflags: image[LOADFLAGS_OFFSET],
+            code32_start: Self::read_u32(&image, CODE32_START_OFFSET)?,
+            initrd_addr_max: Self::read_u32(&image, INITRD_ADDR_MAX_OFFSET)?,
+            kernel_alignment: Self::read_u32(&image, KERNEL_ALIGNMENT_OFFSET)?,
+            cmdline_size: Self::read_u32(&image, CMDLINE_SIZE_OFFSET)?,
             image,
             setup_sects,
             setup_size,
@@ -77,11 +95,11 @@ impl LinuxKernel {
         kernel: &LinuxKernel,
     ) -> Result<(), Box<dyn Error>> {
         // zero the whole page for boot params
-        guest_memory.write(BOOT_PARAMS_ADDR, &[0u8; 4096])?;
+        guest_memory.write(BOOT_PARAMS_ADDR, &[0u8; BOOT_PARAMS_SIZE])?;
         // overlay setup/header
         guest_memory.write(BOOT_PARAMS_ADDR, kernel.setup())?;
         guest_memory.write(
-            BOOT_PARAMS_ADDR + 0x228,
+            BOOT_PARAMS_ADDR + CMDLINE_PTR_OFFSET,
             &(CMDLINE_ADDR as u32).to_le_bytes(),
         )?;
 
